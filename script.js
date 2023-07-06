00f603c39aebcb17f452a5bb7818721327c50278
Saimun Hassan <saimun4u@gmail.com>
2023-07-05 21:42:43 -0400

    Applied finishing touches

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-const changeColor = document.getElementById('changeColor');
+const changeColor = document.getElementById('changeColorBtn');
+
+function generateColor() {
+  let hexColor = '#';
+  let hex_numbers = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+
+  for (let i = 0; i < 6; i++) {
+    let random_index = Math.floor(Math.random() * hex_numbers.length);
+    hexColor += hex_numbers[random_index];
+    console.log(hexColor);
+    document.body.style.background = hexColor;
+  }
+}
+
+changeColorBtn.addEventListener('click', () => {
+  generateColor();
+});
